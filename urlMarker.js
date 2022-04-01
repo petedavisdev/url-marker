60b9ab1c857b25b74070f8167af3c1ecbcbf726e
Pete Davis <pete.davis@rcn.org.uk>
2022-04-01 15:04:26 +0100

    Check for stored marker settings

--- urlMarker.js
+++ urlMarker.js
@@ -1,4 +1,5 @@
-const defaultSettings = JSON.stringify([
+// Default settings for first-time user
+let defaultSettings = JSON.stringify([
 	{
 		name: 'local',
 		urls: ['localhost:', '://local.'],
@@ -6,23 +7,32 @@ const defaultSettings = JSON.stringify([
 	},
 ]);
 
-let settings = defaultSettings;
-
+// Check storage for saved user settings
 chrome.storage.sync.get(['settings'], function (result) {
-	settings = result.settings || defaultSettings;
+	// Create marker using either saved or default settings
+	createMarker(result.settings || defaultSettings);
 });
 
-const marker = JSON.parse(settings).find((item) =>
-	item.urls.some((url) => window.location.href.includes(url))
-);
+function createMarker(settings) {
+	// Find marker settings for current url
+	const marker = JSON.parse(settings).find((item) =>
+		// True if some url is included in the current url
+		item.urls.some((url) => window.location.href.includes(url))
+	);
 
-if (marker) {
+	if (!marker) return;
+
+	// Create marker element
 	const el = document.createElement('span');
 
+	// Add element properties
 	el.id = 'url-marker';
 	el.innerHTML = marker.name;
 	el.style = marker.style;
 
+	// Add marker to page
 	document.body.appendChild(el);
+
+	// Add marker name to title displayed in browser tab
 	document.title = marker.name + ' — ' + document.title;
 }
