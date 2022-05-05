fabae8317a2a277c0e0368556fbd8766cfa5f695
Pete Davis <pete.davis@rcn.org.uk>
2022-05-05 15:34:28 +0100

    Simplify js

--- src/marker.js
+++ src/marker.js
@@ -29,22 +29,22 @@ function createMarker(settings) {
 	// Add the marker to the page
 	document.body.appendChild(el);
 
-	// Dragging -  initial states
-	let dragging = false;
-	let mouseX = 0;
-	let elX = 0;
+	// Drag states
+	let dragging;
+	let mouseX;
+	let elX;
 
-	// Set drag states on mousedown
+	// Set initial drag states on mousedown
 	el.addEventListener('mousedown', (e) => {
 		const elTransform = getComputedStyle(el).getPropertyValue('transform');
 		const elTransformArray = elTransform.replace(/[^0-9\-.,]/g, '').split(',');
 
-		elX = parseFloat(elTransformArray[12] || elTransformArray[4] || 0);
+		elX = +elTransformArray[12] || +elTransformArray[4] || 0;
 		mouseX = e.screenX;
 		dragging = true;
 	});
 
-	// Update transform: translate()
+	// Update transform: translateX(); on mousemove
 	document.addEventListener('mousemove', (e) => {
 		if (dragging) {
 			el.style.transform = `translateX(${e.screenX + elX - mouseX}px)`;
